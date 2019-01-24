7d372838cd0128aa674f97be7e777592cd2ab443
Diórgenes Eugênio <diorgeneseugenio@gmail.com>
2019-01-24 20:04:59 -0200

    Mudança de texto na home.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ola-mundo!');
+    expect(page.getTitleText()).toEqual('Welcome to Olá, mundo!!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ola-mundo'`, () => {
+  it(`should have as title 'Olá, mundo!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ola-mundo');
+    expect(app.title).toEqual('Olá, mundo!');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ola-mundo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Olá, mundo!!');
   });
 });
